7fcc3d52b87ae168e4b759eac524026bcec3fcd7
Stitch <pesterevamb@gmail.com>
2026-02-15 17:36:06 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,6 @@ public class StartUI {
         Item item = new Item();
         DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println("Форматированные даты и время " + item.getCreated().format(formater));
+        System.out.println(item);
     }
 }
